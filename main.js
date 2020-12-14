3499647306a0dc4d6737992e6992c543da5bf9dd
dennisosaj <mail@dennisosaj.de>
2020-12-14 18:43:36 +0100

    changing npm module

--- main.js
+++ main.js
@@ -1,32 +1,34 @@
-const Bitcoin = require('bitcoin-address-generator');
+// http://cryptocoinjs.com/
+var CoinKey = require('coinkey')
+
+// console.log("Private Key (Wallet Import Format): " + ck.privateWif)
+// console.log("Private Key (Hex): " + ck.privateKey.toString('hex'))
+// console.log("Address: " + ck.publicAddress)
 
 var arguments = process.argv.slice(2);
 
 if (arguments.length > 0) {
     if (arguments.length === 2) {
         for (var ii = 0; ii < arguments[0];) {
-            Bitcoin.createWalletAddress(response => {
-                if (response.address.startsWith(arguments[1])) {
-                    console.log('key:' + response.key);
-                    console.log('address:' + response.address);
-                    ii++;
-                }
-            });
+            var ck = new CoinKey.createRandom();
+            if (ck.publicAddress.startsWith(arguments[1])) {
+                console.log('key:' + ck.privateWif);
+                console.log('address:' + ck.publicAddress);
+                ii++;
+            }
         }
     } else if (arguments.length === 1) {
         for (var ii = 0; ii < arguments[0]; ii++) {
-            Bitcoin.createWalletAddress(response => {
-                console.log('key:' + response.key);
-                console.log('address:' + response.address);
-            });
+            var ck = new CoinKey.createRandom();
+            console.log('key:' + ck.privateWif);
+            console.log('address:' + ck.publicAddress);
         }
     } else {
         console.log('1. argument = number to generate key-address combinations');
         console.log('2. argument = address starts with pattern');
     }
 } else {
-    Bitcoin.createWalletAddress(response => {
-        console.log('key:' + response.key);
-        console.log('address:' + response.address);
-    });
+    var ck = new CoinKey.createRandom();
+    console.log('key:' + ck.privateWif);
+    console.log('address:' + ck.publicAddress);
 }
