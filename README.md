344eac1c1f17d60c5347c170ea1f6a94f5e04501
dennisosaj <mail@dennisosaj.de>
2020-12-15 10:15:15 +0100

    Add Performance Indicators

--- README.md
+++ README.md
@@ -5,7 +5,26 @@
 3. run ```node main.js```
 
 4. add additional arguments
-- 1st argument = number to generate key=address combination
-- 2nd argument = address starts with pattern
+    - 1st argument = number to generate key/address combination
+    - 2nd argument = address starts with pattern
 
-Example: ```node main.js 100 1ab```
+Example: 
+
+```node main.js 100 1BG```
+
+Result: 
+
+```
+key:L55Vnx4HPC6jgRmtFh7GCrvnYb133S6RhS39dYbdmV7y4EtBSwaa
+address:1BGC1BbwH2SA9BdSTp62NRVJMYSu7j6Rvb
+...
+key:L3cVT98mgjL4JNZX5Thqr2JjyS5acntrtHGxR9Xfgw5r518TNQwt
+address:1BGkrj1v9rrWuMYa9SfxjBWizEFDij618R
+Pattern: 1BG
+Total found: 100
+Execution time: 8213[ms]
+Total searched: 144418
+Addresses per second: 17585[K/s]
+Performance: 0.0006924344610782589[%]
+
+```
--- main.js
+++ main.js
@@ -9,6 +9,8 @@ var arguments = process.argv.slice(2);
 
 if (arguments.length > 0) {
     if (arguments.length === 2) {
+        var start = new Date().getTime();
+        var total = 0;
         for (var ii = 0; ii < arguments[0];) {
             var ck = new CoinKey.createRandom();
             if (ck.publicAddress.startsWith(arguments[1])) {
@@ -16,19 +18,37 @@ if (arguments.length > 0) {
                 console.log('address:' + ck.publicAddress);
                 ii++;
             }
+            total++;
         }
+        var end  = new Date().getTime();
+        var time = end - start;
+        console.log('Pattern: ' + arguments[1]);
+        console.log('Total found: ' + arguments[0]);
+        console.log('Execution time: ' + time + '[ms]');
+        console.log('Total searched: ' + total);
+        console.log('Addresses per second: ' + Math.ceil(total/(time/1000)) + '[K/s]');
+        console.log('Performance: ' + arguments[0]/total + '[%]');
     } else if (arguments.length === 1) {
+        var start = new Date().getTime();
         for (var ii = 0; ii < arguments[0]; ii++) {
             var ck = new CoinKey.createRandom();
             console.log('key:' + ck.privateWif);
             console.log('address:' + ck.publicAddress);
         }
+        var end  = new Date().getTime();
+        var time = end - start;
+        console.log('Execution time: ' + time + '[ms]');
+        console.log('Addresses per second: ' + Math.ceil(ii/(time/1000)) + '[K/s]');
     } else {
         console.log('1. argument = number to generate key-address combinations');
         console.log('2. argument = address starts with pattern');
     }
 } else {
+    var start = new Date().getTime();
     var ck = new CoinKey.createRandom();
     console.log('key:' + ck.privateWif);
     console.log('address:' + ck.publicAddress);
+    var end  = new Date().getTime();
+    var time = end - start;
+    console.log('Execution time: ' + time + '[ms]');
 }
